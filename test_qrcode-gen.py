e0482e9829a8bd975bef9f47f9aa4afad529695d
Dan Hagon <axiomsofchoice@gmail.com>
2012-04-13 00:58:05 +0100

    Created a module for providing unittests for the qrcode generating script.

---
+++ test_qrcode-gen.py
@@ -0,0 +1,9 @@
+"""Tests the code that generates qrcode PNGs.
+"""
+
+from unittest import TestCase
+
+class QRCodesGenTests(TestCase):
+    """Tests the code that generates qrcode PNGs.
+    """
+    pass
